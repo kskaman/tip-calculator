571367a738daa70a1654923b88f81320f67fefb2
Kamanpreet Singh <kskaman@gmail.com>
2024-12-22 15:25:34 -0330

    corrected regular expressions in useValidation hook

--- src/utils/useValidation.ts
+++ src/utils/useValidation.ts
@@ -1,6 +1,6 @@
 export const useValidation = () => {
-    const validateNumericInput = (value: string): boolean => /^[0-9]8$/.test(value);
-    const validateDecimalInput = (value: string): boolean => /^\d*\.?\d{0, 2}$/.test(value);
+    const validateNumericInput = (value: string): boolean => /^[0-9]*$/.test(value);
+    const validateDecimalInput = (value: string): boolean => /^\d*\.?\d{0,2}$/.test(value);
 
     return { validateNumericInput, validateDecimalInput }
 }
